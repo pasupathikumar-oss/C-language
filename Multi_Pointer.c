ab9494b3019073c73f326e9f701a98409d7d1561
pasupathikumar-oss <pasupathi.kumar@vvdntech.in>
2026-07-20 11:08:18 +0000

    created Multi_Pointer.c

---
+++ Multi_Pointer.c
@@ -0,0 +1,23 @@
+/*  | Pointer type | To get value |
+    | ------------ | ------------ |
+    | `int *`      | `*p`         |
+    | `int **`     | `**p`        |
+    | `int ***`    | `***p`       |
+    | `int ****`   | `****p`      |
+    | `int *****`  | `*****p`     |
+*/
+
+#include <stdio.h>
+
+int main() {
+    int var = 10;
+    // Pointer to int
+    int *ptr1 = &var;
+    // Pointer to pointer (double pointer)
+    int **ptr2 = &ptr1;  
+    // Accessing values using all three
+    printf("var: %d\n", var);          
+    printf("*ptr1: %d\n", *ptr1);
+    printf("**ptr2: %d", **ptr2);
+    return 0;
+}
